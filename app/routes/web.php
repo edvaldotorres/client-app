b243bfc7bc18da2d7aded2ab5cd9d579f941a802
edvaldotorres <edvaldo.torres@tisaude.com>
2023-04-24 21:50:46 -0300

    :recycle: Route for client

--- app/routes/web.php
+++ app/routes/web.php
@@ -21,8 +21,6 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
 Route::middleware('auth')->prefix('admin')->name('admin.')->group(function () {
     Route::resource('clients', ClientController::class);
 });
