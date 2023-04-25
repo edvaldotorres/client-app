58f03f8f9075adc0372b797c6956b1193c2dd14d
edvaldotorres <edvaldo.torres@tisaude.com>
2023-04-24 21:30:02 -0300

    :sparkles: Add client model get & set atributtes

--- app/app/Models/Client.php
+++ app/app/Models/Client.php
@@ -22,4 +22,50 @@ class Client extends Model
         'image',
         'name_social',
     ];
+
+    /**
+     * > The `getDateOfBirthAttribute` function is called when you try to access the `date_of_birth` attribute
+     * of a `User` object
+     * 
+     * @param value The value of the attribute.
+     * 
+     * @return The birth date of the user.
+     */
+    public function getDateOfBirthAttribute($value)
+    {
+        return date('d/m/Y', strtotime($value));
+    }
+
+    /**
+     * > The `setDateOfBirthAttribute` function is called when the `date_of_birth` attribute is set
+     * 
+     * @param value The value of the attribute.
+     */
+    public function setDateOfBirthAttribute($value)
+    {
+        $this->attributes['date_of_birth'] = date('Y-m-d', strtotime($value));
+    }
+
+    /**
+     * > The `getDocumentAttribute` function is called when you try to access the `document` attribute
+     * of a `User` object
+     * 
+     * @param value The value of the attribute.
+     * 
+     * @return The document of the user.
+     */
+    public function getDocumentAttribute($value)
+    {
+        return preg_replace('/(\d{3})(\d{3})(\d{3})(\d{2})/', '$1.$2.$3-$4', $value);
+    }
+
+    /**
+     * > The `setDocumentAttribute` function is called when the `document` attribute is set
+     * 
+     * @param value The value of the attribute.
+     */
+    public function setDocumentAttribute($value)
+    {
+        $this->attributes['document'] = preg_replace('/[^0-9]/', '', $value);
+    }
 }
