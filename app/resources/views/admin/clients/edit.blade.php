306e9b36e1b7271814b8dd2a472ad41bad1d0c4c
edvaldotorres <edvaldo.torres@tisaude.com>
2023-04-26 01:38:58 -0300

    Revert ":recycle: View Client - Edit"
    This reverts commit aacf965d489f8ed0b62e6dccc60afff91dede803.

--- app/resources/views/admin/clients/edit.blade.php
+++ app/resources/views/admin/clients/edit.blade.php
@@ -16,38 +16,25 @@
                                     <label for="name_client" class="form-label">Nome:</label>
                                     <input type="text" class="form-control" name="name_client"
                                         value="{{ $client->name_client }}">
-                                    @error('name_client')
-                                        <div class="text-danger">{{ $message }}</div>
-                                    @enderror
                                 </div>
                                 <div class="col-md-6">
                                     <label for="date_of_birth" class="form-label">Data de Nascimento:</label>
                                     <div class="input-group">
-                                        <input type="text" class="form-control mask-date_of_birth" name="date_of_birth"
-                                            placeholder="Ex: 00/00/0000"
-                                            value="{{ old('date_of_birth') ?? $client->date_of_birth }}">
+                                        <input type="date" class="form-control" name="date_of_birth"
+                                            value="{{ $client->date_of_birth }}">
                                     </div>
-                                    @error('date_of_birth')
-                                        <div class="text-danger">{{ $message }}</div>
-                                    @enderror
                                 </div>
                             </div>
                             <div class="row mb-3">
                                 <div class="col-md-6">
                                     <label for="document" class="form-label">CPF/CNPJ:</label>
-                                    <input type="text" class="form-control" name="document" id="document"
+                                    <input type="text" class="form-control" name="document"
                                         value="{{ $client->document }}">
-                                    @error('document')
-                                        <div class="text-danger">{{ $message }}</div>
-                                    @enderror
                                 </div>
                                 <div class="col-md-6">
                                     <label for="name_social" class="form-label">Nome Social:</label>
                                     <input type="text" class="form-control" name="name_social"
                                         value="{{ $client->name_social }}">
-                                    @error('name_social')
-                                        <div class="text-danger">{{ $message }}</div>
-                                    @enderror
                                 </div>
                             </div>
                             <div class="row mb-3">
@@ -56,9 +43,6 @@
                                     <div class="mb-3">
                                         <input type="file" class="form-control" name="image">
                                     </div>
-                                    @error('image')
-                                        <div class="text-danger">{{ $message }}</div>
-                                    @enderror
                                     <div class="mb-3">
                                         <img src="{{ $client->image ? asset('storage/images/' . $client->image) : asset('images/default.png') }}"
                                             alt="Foto do Cliente" class="img-thumbnail img-fluid">
@@ -74,19 +58,12 @@
     </div>
 
     <script>
-        $(document).ready(function() {
-            $('#document').keydown(function(event) {
-                const document = $(this).val();
-                const length = document.length;
-
-                if (length < 14) {
-                    $(this).mask('000.000.000-009');
-                } else {
-                    $(this).mask('00.000.000/0000-00');
-                }
-            });
-
-            $('.mask-date_of_birth').mask('00/00/0000');
+        $('#document').mask('000.000.000-00', {
+            onKeyPress: function(document, e, field, options) {
+                const masks = ['000.000.000-000', '00.000.000/0000-00'];
+                const mask = (document.length > 14) ? masks[1] : masks[0];
+                $('#document').mask(mask, options);
+            }
         });
     </script>
 @endsection
